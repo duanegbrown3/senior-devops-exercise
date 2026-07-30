2b182e3a16c3b9b6a503925f7b202ef8bab8414e
duanegbrown3 <duanegbrown3@gmail.com>
2026-07-30 12:31:34 +0100

    feat(terraform): add Azure resource group module

---
+++ terraform/main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_resource_group" "platform" {
+
+  name     = var.resource_group_name
+  location = var.location
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "location" {
+  type = string
+  default = "UK South"
+}
+
+variable "resource_group_name" {
+  type = string
+  default = "rg-devops-platform"
+}
+
+variable "acr_name" {
+  type = string
+}
